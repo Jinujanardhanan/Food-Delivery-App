a313b41d5554405574d08fe0ea98d6916403f671
Jinumon A J <jinujanardhanan03@gmail.com>
2024-01-18 10:39:52 +0530

    live API data is fetched

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,15 +8,15 @@ const RestaurantCard = (props) => {
     cuisines,
     avgRating,
     costForTwo,
-    deliveryTime} = resData?.data;
+    sla} = resData?.info;
   return(
     <div className="res-card">
       <img className="res-logo" src={CDN_URL+ cloudinaryImageId}/>
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating}stars</h4>
-      <h4>{costForTwo/100}For Two</h4>
-      <h4>{deliveryTime}minutes </h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime} minutes </h4>
     </div>
   )
 };
